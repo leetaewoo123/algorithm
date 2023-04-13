abe86024a31b19723190705922ff19e44aaa9191
taewoo1113 <96154411+leetaewoo123@users.noreply.github.com>
2023-04-13 09:24:38 +0900

    [level 1] Title: 폰켓몬, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,9 @@
 def solution(nums):
     answer = 0
-    get = len(nums)/2
-    nums = list(set(nums))
-    if (len(nums) > get) :
+    get = len(nums)/2      # 가질 수 있는 포켓몬 수
+    nums = list(set(nums)) # nums의 중복값 제거 후 리스트로 만듬
+    if (len(nums) > get) : 
+        # 경우의 수가 아니라 선택할 수 있는 포켓몬 종류 개수의 최댓값을 구하는 것이기 때문에 get과 nums를 비교 
         answer = get
     else : answer = len(nums)
     return answer
