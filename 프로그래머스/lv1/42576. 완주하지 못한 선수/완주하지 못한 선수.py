3e5741613060f898e53fa8a81977310b8cf9c9d3
taewoo1113 <96154411+leetaewoo123@users.noreply.github.com>
2023-04-19 08:49:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.36 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -7,3 +7,13 @@ def solution(participant, completion):
             return participant[i]
     
     return participant[len(participant)-1]
+
+# 첫 코드 : 출력은 맞았지만, 효율이 떨어짐
+#def solution(participant, completion):
+#    
+#    for i in completion:
+#        if i in participant:
+#            participant.remove(i)
+#            
+#    return participant[0]
+
