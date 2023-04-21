38ac0746cda8a9a9d9af162be968a9b607ed4145
taewoo1113 <96154411+leetaewoo123@users.noreply.github.com>
2023-04-21 22:58:45 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.45 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,42 @@
+def solution(numbers, hand):
+    answer = ''
+    keypad = [[1,4,7,"*"],[2,5,8,0],[3,6,9,'#']]
+    lh,rh = "*","#"
+    
+    for i in numbers:
+        if i in keypad[0]:
+            answer += "L"
+            lh = i
+        elif i in keypad[2]:
+            answer += "R"
+            rh = i
+        else :
+            for o in range (3):
+                for j in range (4):
+                    if keypad[o][j] == i :
+                        hl = [o,j]
+                    if keypad[o][j] == lh :
+                        ll = [o,j]
+                    elif keypad[o][j] == rh :
+                        rl = [o,j]
+            rt = abs(rl[0]-hl[0])+abs(rl[1]-hl[1])
+            lt = abs(ll[0]-hl[0])+abs(ll[1]-hl[1])
+            
+            if rt > lt:
+                answer += "L"
+                lh = i
+            elif rt < lt :
+                answer += "R"
+                rh = i
+            else :
+                if hand == "right":
+                    answer += "R"
+                    rh = i
+                else :
+                    answer += "L"
+                    lh = i
+                
+    return answer
+
+
+## LRLLLR
