e253ab381e44b8939c38ca85204c01c85c458775
taewoo1113 <96154411+leetaewoo123@users.noreply.github.com>
2023-04-25 13:31:58 +0900

    Delete 프로그래머스/lv0/120583. 중복된 숫자 개수 directory

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++
@@ -1,9 +0,0 @@
-function solution(array, n) {
-    var answer = 0;
-    for (let i=0; i<=array.length; i++){
-        if (array[i]==n){
-            answer +=1;
-        }
-    }
-    return answer;
-}
