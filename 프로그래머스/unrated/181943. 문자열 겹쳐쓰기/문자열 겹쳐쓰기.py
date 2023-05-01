47eff980c881a25e4dbc5f8a57526460ab1a0f9a
taewoo1113 <96154411+leetaewoo123@users.noreply.github.com>
2023-05-01 10:06:15 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,7 @@
+def solution(my_string, overwrite_string, s):
+    answer = ''
+    if len(my_string)==len(overwrite_string):
+        return overwrite_string
+    else:
+        return my_string[:s]+overwrite_string+my_string[s+len(overwrite_string):]
+    return 
